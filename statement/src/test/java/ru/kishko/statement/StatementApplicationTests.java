7b28a0c4bc0d2f58969d48400a6df1651495899f
d3k0d4nce <kishko.2003@list.ru>
2026-04-26 00:56:35 +0300

    Добавление трассировки

--- statement/src/test/java/ru/kishko/statement/StatementApplicationTests.java
+++ statement/src/test/java/ru/kishko/statement/StatementApplicationTests.java
@@ -4,6 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "otel.sdk.disabled=true",
+        "otel.traces.exporter=none",
+        "otel.metrics.exporter=none"
+})
+
 class StatementApplicationTests {
 
     @Test
