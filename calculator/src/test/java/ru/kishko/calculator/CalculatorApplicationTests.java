ac355a4620eae4c0f84fd9773077ec9d06d82825
d3k0d4nce <kishko.2003@list.ru>
2026-04-26 00:38:26 +0300

    Добавление трассировки

--- calculator/src/test/java/ru/kishko/calculator/CalculatorApplicationTests.java
+++ calculator/src/test/java/ru/kishko/calculator/CalculatorApplicationTests.java
@@ -2,8 +2,13 @@ package ru.kishko.calculator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "otel.traces.exporter=none",
+        "otel.metrics.exporter=none"
+})
 class CalculatorApplicationTests {
 
 	@Test
--- gateway/src/test/java/ru/kishko/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/ru/kishko/gateway/GatewayApplicationTests.java
@@ -2,8 +2,13 @@ package ru.kishko.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "otel.traces.exporter=none",
+        "otel.metrics.exporter=none"
+})
 class GatewayApplicationTests {
 
     @Test
