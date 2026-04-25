af687996909ba1e89d093fa6d8f1e221d1cc9c34
d3k0d4nce <kishko.2003@list.ru>
2026-04-26 01:01:48 +0300

    Добавление трассировки

--- dossier/src/test/java/ru/kishko/dossier/DossierApplicationTests.java
+++ dossier/src/test/java/ru/kishko/dossier/DossierApplicationTests.java
@@ -4,11 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-		"otel.sdk.disabled=true",
-		"otel.traces.exporter=none",
-		"otel.metrics.exporter=none"
-})
 class DossierApplicationTests {
 
 	@Test
